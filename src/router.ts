70281f999feaeea29108a434a11ddc9442e96e81
dannyHTML <dannyjag@gmail.com>
2023-12-22 16:45:29 +0000

    name change

--- src/router.ts
+++ src/router.ts
@@ -6,22 +6,22 @@ export const router = createRouter({
         {
         path: "/",
         name: "Personal-info",
-        component: () => import('./pages/step1.vue')
+        component: () => import('./pages/Step1.vue')
         },
         {
         path: "/Select-your-plan",
         name: "Select-your-plan",
-        component: () => import('./pages/step2.vue')
+        component: () => import('./pages/Step2.vue')
         },
         {
         path: "/Pick-add-ons",
         name: "Pick-add-ons",
-        component: () => import('./pages/step3.vue')
+        component: () => import('./pages/Step3.vue')
         },        
         {
         path: "/Finishing-up",
         name: "Finishing-up",
-        component: () => import('./pages/step4.vue')
+        component: () => import('./pages/Step4.vue')
         }
     ]
 })
